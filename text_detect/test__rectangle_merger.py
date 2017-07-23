c096f8f875e5a2e0a8ca9b3b5a856196b90628ff
Lin Fan <flin@thoughtworks.com>
2017-07-23 21:56:09 +0800

    rename test funcs

--- text_detect/test__rectangle_merger.py
+++ text_detect/test__rectangle_merger.py
@@ -10,16 +10,16 @@ class TestRectangleMerger(TestCase):
     rect_c = Rectangle(4, 6, 3, 2)
     merger = Merger()
 
-    def test_merge_rectangle_to_pool(self):
+    def test__merge_rectangle_to_pool(self):
         self.assertTrue(self.merger._is_rectangles_overlapped(self.rect_a, self.rect_b))
         self.assertFalse(self.merger._is_rectangles_overlapped(self.rect_a, self.rect_c))
 
-    def test_merge_2_rectangles(self):
+    def test__merge_2_rectangles(self):
         rect = self.merger._merge_2_rectangles(self.rect_a, self.rect_b)
         expect_rect = Rectangle(1, 1, 4, 3)
         self.assertEqual(rect, expect_rect, "%s and %s not equal!" % (rect, expect_rect))
 
-    def test_merge_all_rectangles(self):
+    def test__merge_all_rectangles(self):
         rects = [self.rect_a, self.rect_b, self.rect_c]
         merged_rects = self.merger.merge_rectangle_list(rects)
         expect_rects = [Rectangle(1, 1, 4, 3), Rectangle(4, 6, 3, 2)]
