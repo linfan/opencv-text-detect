fcdb9ea0938dcad69057a6c4641c2290386cca0a
Lin Fan <flin@thoughtworks.com>
2017-07-24 18:15:17 +0800

    merge horizontally close-by rectangles

--- text_detect/rectangle_merger.py
+++ text_detect/rectangle_merger.py
@@ -7,6 +7,15 @@ class RectangleMerger:
         return not (rect_1.x2 < rect_2.x1 or rect_1.y2 < rect_2.y1
                     or rect_1.x1 > rect_2.x2 or rect_1.y1 > rect_2.y2)
 
+    def _is_horizontally_near_or_overlapped(self, rect_1, rect_2):
+        horizontal_distance_rate = 0.15
+        w_1 = (rect_1.x2 - rect_1.x1) * horizontal_distance_rate
+        w_2 = (rect_2.x2 - rect_2.x1) * horizontal_distance_rate
+        return (rect_2.x1 - w_2 < rect_1.x1 < rect_2.x2 + w_2) or \
+               (rect_2.x1 - w_2 < rect_1.x2 < rect_2.x2 + w_2) or \
+               (rect_1.x1 - w_1 < rect_2.x1 < rect_1.x2 + w_1) or \
+               (rect_1.x1 - w_1 < rect_2.x1 < rect_1.x2 + w_1)
+
     def _is_vertically_included(self, rect_1, rect_2):
         return (rect_2.y1 < rect_1.y1 and rect_2.y2 > rect_1.y2) or \
                (rect_1.y1 < rect_2.y1 and rect_1.y2 > rect_2.y2)
@@ -26,7 +35,7 @@ class RectangleMerger:
                rect_2.get_height() * max_vertical_diff_rate > rect_1.get_height()
 
     def _is_rectangles_overlapped_horizontally(self, rect_1, rect_2):
-        return self._is_rectangles_overlapped(rect_1, rect_2) and \
+        return self._is_horizontally_near_or_overlapped(rect_1, rect_2) and \
                self._is_height_of_rects_match(rect_1, rect_2) and \
                (self._is_vertically_near_eachother(rect_1, rect_2) or
                 self._is_vertically_included(rect_1, rect_2))
@@ -35,7 +44,8 @@ class RectangleMerger:
         return Rectangle.from_2_pos(rect_1.x1 < rect_2.x1 and rect_1.x1 or rect_2.x1,
                                     rect_1.y1 < rect_2.y1 and rect_1.y1 or rect_2.y1,
                                     rect_1.x2 > rect_2.x2 and rect_1.x2 or rect_2.x2,
-                                    rect_1.y2 > rect_2.y2 and rect_1.y2 or rect_2.y2)
+                                    rect_1.y2 > rect_2.y2 and rect_1.y2 or rect_2.y2,
+                                    rect_1.merged_times + rect_2.merged_times + 1)
 
     def _merge_rectangle_to_pool(self, rect_pool):
         new_pool = []
--- text_detect/test__rectangle_merger.py
+++ text_detect/test__rectangle_merger.py
@@ -5,23 +5,23 @@ from text_detect.rectangle import Rectangle
 
 
 class TestRectangleMerger(TestCase):
-    rect_a = Rectangle(1, 1, 3, 2)
-    rect_b = Rectangle(2, 2, 3, 2)
-    rect_c = Rectangle(4, 6, 3, 2)
+    rect_a = Rectangle.from_2_pos(1, 1, 10, 5)
+    rect_b = Rectangle.from_2_pos(11, 1.2, 13, 5.2)
+    rect_c = Rectangle.from_2_pos(2, 3, 20, 10)
     merger = Merger()
 
     def test__merge_rectangle_to_pool(self):
-        self.assertTrue(self.merger._is_rectangles_overlapped(self.rect_a, self.rect_b))
-        self.assertFalse(self.merger._is_rectangles_overlapped(self.rect_a, self.rect_c))
+        self.assertTrue(self.merger._is_rectangles_overlapped(self.rect_a, self.rect_c))
+        self.assertFalse(self.merger._is_rectangles_overlapped(self.rect_a, self.rect_b))
 
     def test__merge_2_rectangles(self):
-        rect = self.merger._merge_2_rectangles(self.rect_a, self.rect_b)
-        expect_rect = Rectangle(1, 1, 4, 3)
+        rect = self.merger._merge_2_rectangles(self.rect_a, self.rect_c)
+        expect_rect = Rectangle.from_2_pos(1, 1, 20, 10)
         self.assertEqual(rect, expect_rect, "%s and %s not equal!" % (rect, expect_rect))
 
     def test__merge_all_rectangles(self):
         rects = [self.rect_a, self.rect_b, self.rect_c]
         merged_rects = self.merger.merge_rectangle_list(rects)
-        expect_rects = [Rectangle(1, 1, 4, 3), Rectangle(4, 6, 3, 2)]
+        expect_rects = [Rectangle.from_2_pos(1, 1, 13, 5.2), Rectangle.from_2_pos(2, 3, 20, 10)]
         self.assertListEqual(merged_rects, expect_rects, " ".join([r.__str__() for r in merged_rects]))
 
