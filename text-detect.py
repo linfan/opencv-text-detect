36d8e9bd6bb65fa4851bf7c01c90780c39079180
Lin Fan <flin@thoughtworks.com>
2017-07-29 19:39:29 +0800

    detect text content

--- text-detect.py
+++ text-detect.py
@@ -4,6 +4,8 @@ from flask import Flask, render_template, send_from_directory, request
 import urllib.request
 import urllib.error
 import socket
+import os
+import re
 from text_detect.io_handler import IoHandler
 from text_detect.rectangle_detector import RectangleDetector as Detector
 from text_detect.rectangle_merger import RectangleMerger as Merger
@@ -26,62 +28,85 @@ def favicon():
 
 @app.route("/detect/<path:url>")
 def hello(url):
-    message = ''
+    messages = []
     success = True
 
     saved_image_name = url.split('/')[-1]
     saved_image_path = '%s/%s' % (static_folder, saved_image_name)
-    msg, suc = download_image(saved_image_path, url)
-    message, success = update_msg_and_suc(message, msg, success, suc)
+    success, messages = download_image(saved_image_path, url, success, messages)
 
     saved_image_name_without_postfix = saved_image_name.split('.')[0]
-    output_folder = '%s/%s/' % (static_folder, saved_image_name_without_postfix)
+    output_folder = '%s/%s' % (static_folder, saved_image_name_without_postfix)
     result_image_url = detect_text_area(saved_image_path, output_folder)
 
-    part_image_file = '%s/part-0%d.jpg' % (output_folder, 1)
-    detect_res = subprocess.run(["tesseract", part_image_file, "stdout", "--oem", "1", "--psm", "13"],
-                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    message, success = update_msg_and_suc(message, detect_res.stderr, success, detect_res.returncode == 0)
+    part_image_files = ls_dir(output_folder, r'.*part-[0-9]+\.jpg')
+    for part in part_image_files:
+        success, messages = recognize_text(part, output_folder, success, messages)
+
     if success:
-        message = detect_res.stdout
+        part_txt_files = ls_dir(output_folder, r'.*part-[0-9]+\.txt')
+        for part in part_txt_files:
+            messages.append(read_recognized_txt(part, output_folder))
 
-    return render_template('res.html', success=success, image=result_image_url, message=message)
+    return render_template('res.html', success=success, image=result_image_url, messages=messages)
 
 
-def update_msg_and_suc(message, msg, success, suc):
-    success &= suc
+def read_recognized_txt(part, output_folder):
+    part_file_path = '%s/%s' % (output_folder, part)
+    txt_fd = open(part_file_path)
+    line = txt_fd.readline()  # just the first line
+    txt_fd.close()
+    return re.sub(r'^[^0-9a-zA-Z]*', '', re.sub(r'[^0-9a-zA-Z]*$', '', line))
+
+
+def ls_dir(output_folder, pattern):
+    return [f for f in os.listdir(output_folder) if re.match(pattern, f)]
+
+
+def recognize_text(part, output_folder, success, messages):
+    suc, msg = True, ''
+    part_file_path = '%s/%s' % (output_folder, part)
+    detect_txt_file_path = '%s/%s' % (output_folder, part.replace('.jpg', ''))
+    if not os.path.exists('%s.txt' % detect_txt_file_path):
+        detect_res = subprocess.run(["tesseract", part_file_path, detect_txt_file_path, "--oem", "1", "--psm", "13"],
+                                    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        suc, msg = (detect_res.returncode == 0), detect_res.stderr
+    return update_msg_and_suc(success, suc, messages, msg)
+
+
+def update_msg_and_suc(success, suc, messages, msg):
     if not suc:
-        message += '<li>%s</li>' % msg
-    return message, success
+        messages.append(msg)
+    return (success and suc), messages
 
 
-def download_image(saved_image_path, url):
-    message = ''
-    socket.setdefaulttimeout(3)
-    try:
-        urllib.request.urlretrieve(url, saved_image_path)
-        success = True
-    except (urllib.error.HTTPError, urllib.error.URLError):
-        success = False
-        message = 'Failed to download image %s' % url
-    return message, success
+def download_image(saved_image_path, url, success, messages):
+    suc, msg = True, ''
+    if not os.path.exists(saved_image_path):
+        try:
+            socket.setdefaulttimeout(3)
+            urllib.request.urlretrieve(url, saved_image_path)
+        except (urllib.error.HTTPError, urllib.error.URLError):
+            suc, msg = False, 'Failed to download image %s' % url
+    return update_msg_and_suc(success, suc, messages, msg)
 
 
 def detect_text_area(input_file, output_path):
-    # Load image
-    io_handler = IoHandler()
-    io_handler.input_file = input_file
-    io_handler.output_path = output_path
-    img = io_handler.read_image()
-    # Detect text area
-    detector = Detector()
-    rectangles = detector.find_all_text_rectangles(img)
-    # Merge rectangles
-    merger = Merger()
-    rectangles = merger.merge_rectangle_list(rectangles)
-    # Select rectangles
-    selector = Selector()
-    rectangles = selector.select_according_to_merged_times(rectangles)
-    # Save result
-    io_handler.write_result(img, rectangles)
-    return '%s%sresult.jpg' % (request.url_root, io_handler.output_path)
+    if not os.path.exists('%s/result.jpg' % output_path):
+        # Load image
+        io_handler = IoHandler()
+        io_handler.input_file = input_file
+        io_handler.output_path = '%s/' % output_path
+        img = io_handler.read_image()
+        # Detect text area
+        detector = Detector()
+        rectangles = detector.find_all_text_rectangles(img)
+        # Merge rectangles
+        merger = Merger()
+        rectangles = merger.merge_rectangle_list(rectangles)
+        # Select rectangles
+        selector = Selector()
+        rectangles = selector.select_according_to_merged_times(rectangles)
+        # Save result
+        io_handler.write_result(img, rectangles)
+    return '%s%s/result.jpg' % (request.url_root, output_path)
