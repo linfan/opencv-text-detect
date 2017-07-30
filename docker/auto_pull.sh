e0d2d0178ca88b9fc5a6d411f2a091afb6c46502
Lin Fan <flin@thoughtworks.com>
2017-07-30 23:50:04 +0800

    fix auto_pull judgement error

--- docker/auto_pull.sh
+++ docker/auto_pull.sh
@@ -4,13 +4,16 @@ if [ $# -lt 1 ]; then
 fi
 
 cd ${1}
-PULL_RES=`git pull | grep 'up-to-date'`
-if [ $? -eq 0 ] && [ "${PULL_RES}" == "" ]; then
-    docker tag t4 t4-delete
-    docker rmi t4
-    docker build -t t4 .
-    docker rm -f t4
-    docker run -dt --name t4 -p 6000:5000 t4
-    docker rmi t4-delete
+PULL_RES=`git pull`
+if [ $? -eq 0 ]; then
+    CHECK_RES=`echo $PULL_RES | grep 'up-to-date'`
+    if [ "${CHECK_RES}" == "" ]; then
+        docker tag t4 t4-delete
+        docker rmi t4
+        docker build -t t4 .
+        docker rm -f t4
+        docker run -dt --name t4 -p 6000:5000 t4
+        docker rmi t4-delete
+    fi
 fi
-cd -
+cd - >/dev/null
