5165d15a9ed3849398008d418de4b6897fbdfc13
Lin Fan <flin@thoughtworks.com>
2017-08-06 00:07:44 +0800

    allow specify output base folder

--- text-detect.sh
+++ text-detect.sh
@@ -4,23 +4,26 @@ if [ "${1}" = "" ]; then
     echo 'Usage: ./text-detect.sh <image-path>'
     exit -1
 fi
+if [ "${2}" != "" ]; then
+    OUT_BASE_FOLDER="${2%%/}/"    # remove ending '/' and put another '/'
+fi
 
 FILE_PATH=${1}
-OUT_DIR=`basename ${FILE_PATH%.*}`
+OUT_FOLDER=${OUT_BASE_FOLDER}`basename ${FILE_PATH%.*}`
 IMAGE_FORMAT=${FILE_PATH##*.}
 
 echo "Read image: ${FILE_PATH}"
-echo "Write folder: ${OUT_DIR}"
+echo "Write folder: ${OUT_FOLDER}"
 
-rm -fr ${OUT_DIR}
-python3 text_detect_wrap.py ${FILE_PATH} ${OUT_DIR}
-for part in `ls ${OUT_DIR}/part-*.jpg`; do
+rm -fr ${OUT_FOLDER}
+python3 text_detect_wrap.py ${FILE_PATH} ${OUT_FOLDER}
+for part in `ls ${OUT_FOLDER}/part-*.jpg`; do
     num=${part##*-}
-    tesseract ${part} ${OUT_DIR}/result-${num} --oem 1 --psm 13
-    cat ${OUT_DIR}/result-${num}.txt >> ${OUT_DIR}/result.txt
+    tesseract ${part} ${OUT_FOLDER}/result-${num} --oem 1 --psm 13
+    cat ${OUT_FOLDER}/result-${num}.txt >> ${OUT_FOLDER}/result.txt
 done
-cat ${OUT_DIR}/result.txt | sed -e 's/^[^a-zA-Z0-9]*//' -e 's/[^a-zA-Z0-9]*$//' > ${OUT_DIR}/result.trim.txt
+cat ${OUT_FOLDER}/result.txt | sed -e 's/^[^a-zA-Z0-9]*//' -e 's/[^a-zA-Z0-9]*$//' > ${OUT_FOLDER}/result.trim.txt
 echo "---- RESULT ----"
-cat ${OUT_DIR}/result.trim.txt
+cat ${OUT_FOLDER}/result.trim.txt
 echo "----------------"
-cp ${FILE_PATH} ${OUT_DIR}/origin.${IMAGE_FORMAT}
+cp ${FILE_PATH} ${OUT_FOLDER}/origin.${IMAGE_FORMAT}
